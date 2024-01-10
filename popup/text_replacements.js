6f0d8d5e957e6b2802c98b0db4c20e516e2eec72
Saksham Karnawat <sakshamkarnawat0@gmail.com>
2024-01-10 15:50:25 +0530

    Refactored code, added proper README

--- popup/text_replacements.js
+++ popup/text_replacements.js
@@ -1,14 +1,6 @@
-// function handleResponse(message) {
-//     console.log(`Message from the background script: ${message.response}`);
-// }
-
-// function handleError(error) {
-//     console.log(`Error: ${error}`);
-// }
-
 const addButton = document.getElementById("add");
-var key = document.getElementById("key");
-var value = document.getElementById("value");
+const key = document.getElementById("key");
+const value = document.getElementById("value");
 
 document.addEventListener('DOMContentLoaded', function () {
     getTextReplacements().then((data) => {
@@ -48,7 +40,9 @@ function getTextReplacements() {
 function addTextReplacement(key, value) {
     getTextReplacements().then(async (data) => {
         const replacements = data.replacements || [];
+
         replacements.push({ key, value });
+
         await browser.storage.local.set({ replacements: replacements });
 
         if (key && value) {
@@ -76,8 +70,6 @@ function addTextReplacement(key, value) {
         }
 
         clearInputFields();
-
-
     });
 }
 
@@ -90,7 +82,6 @@ function deleteRow(row) {
 
     browser.storage.local.get('replacements').then(async (data) => {
         const replacements = data.replacements || {};
-
         for (let i = 0; i < replacements.length; i++) {
             if (replacements[i].key === keyToDelete) {
                 replacements.splice(i, 1);
@@ -99,7 +90,6 @@ function deleteRow(row) {
         }
 
         await browser.storage.local.set({ replacements: replacements });
-
     });
 }
 
@@ -107,36 +97,3 @@ function clearInputFields() {
     key.value = "";
     value.value = "";
 }
-
-// function listenForClicks() {
-
-//     addButton.addEventListener("click", (e) => {
-//         console.log("key: ", key.value);
-//         console.log("value: ", value.value);
-
-//         browser.runtime.sendMessage({
-//             command: "addTextReplacement",
-//             key: document.getElementById("key").value,
-//             value: document.getElementById("value").value
-//         });
-
-//         // adding.then(handleResponse, handleError);
-//     });
-// }
-
-
-// function reportExecuteScriptError(error) {
-//     document.querySelector("#popup-content").classList.add("hidden");
-//     document.querySelector("#error-content").classList.remove("hidden");
-//     console.error(`Failed to execute text_replacements content script: ${error.message}`);
-// }
-
-
-// /**
-// * When the popup loads, inject a content script into the active tab,
-// * and add a click handler.
-// * If we couldn't inject the script, handle the error.
-// */
-// // browser.tabs.executeScript({ file: "/content_scripts/storage.js" })
-// //     .then(listenForClicks)
-// //     .catch(reportExecuteScriptError);
